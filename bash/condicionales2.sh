947a9fe5f487809973e035f583bcd57a47584526
NEBULA1978 <rpascualey@gmail.com>
2023-02-27 01:47:54 +0100

    Añadiendo a archivo condicionales2.sh ejemplos2

--- bash/condicionales2.sh
+++ bash/condicionales2.sh
@@ -4,6 +4,7 @@ echo " CONDICINALES "
 name=""
 age=0
 year=0
+count=0
 
 read -p "INgresa tu nombre: " name
 read -p "INgresa tu edad: " age
@@ -12,79 +13,56 @@ echo "Hola mi nombre es $name y tengo $age años."
 
 echo "======================================"
 
-#Creamos condicion:
+#Creamos una condición:
 if (($age <= 18)); then
     echo "$name, Eres menor de edad"
-else
-    echo "$name, Eres mayor de edad"
-fi
-
-echo "======================================"
-
-#Creamos una condición:
-if [ $age -lt 18 ]; then
-    echo "$name, No puedes votar por ser menor de edad"
-elif [ $age -gt 65 ]; then
-    echo "$name, Puedes votar como persona mayor"
-else
+    ((count++))
+elif (($age >= 18)) && (($age <= 65)); then
     echo "$name, Puedes votar"
+    ((count++))
+elif (($age > 65)); then
+    echo "$name, Puedes votar como persona mayor"
+    ((count++))
 fi
 
-echo "======================================"
-
-#Creamos una condición:
 if [ $year -eq 2022 ]; then
     echo "$name, Este año es el año de las elecciones presidenciales"
+    ((count++))
 elif [ $year -gt 2022 ]; then
     echo "$name, Las elecciones presidenciales ya pasaron"
-else
+    ((count++))
+elif [ $year -lt 2022 ]; then
     echo "$name, Las elecciones presidenciales aún no han llegado"
+    ((count++))
 fi
 
-echo "======================================"
-
-#Creamos una condición:
 if [ $age -ne 35 ]; then
     echo "$name, Tu edad es diferente a 35 años"
-else
-    echo "$name, Tu edad es igual a 35 años"
+    ((count++))
 fi
 
-echo "======================================"
-
-#Creamos una condición:
 if [ $age -gt 30 ]; then
     echo "$name, Tu edad es mayor a 30 años"
-else
-    echo "$name, Tu edad es menor o igual a 30 años"
+    ((count++))
 fi
 
-echo "======================================"
-
-#Creamos una condición:
 if [ $age -ge 18 ]; then
     echo "$name, Tu edad es mayor o igual a 18 años"
-else
-    echo "$name, Tu edad es menor a 18 años"
+    ((count++))
 fi
 
-echo "======================================"
-
-#Creamos una condición:
 if [ $age -lt 40 ]; then
     echo "$name, Tu edad es menor a 40 años"
-else
-    echo "$name, Tu edad es mayor o igual a 40 años"
+    ((count++))
+fi
+
+if [ $age -le 25 ]; then
+    echo "$name, Tu edad es menor o igual a 25 años"
+    ((count++))
 fi
 
 echo "======================================"
-
-#Creamos una condición:
-if [ $age -le 25 ]; then
-    echo "$name, Tu edad es menor o igual a 25 años"
-else
-    echo "$name, Tu edad es mayor a 25 años"
-fi
+echo "Se cumplieron $count condiciones"
 
 #<>
 
