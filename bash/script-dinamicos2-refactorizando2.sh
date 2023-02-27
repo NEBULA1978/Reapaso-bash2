c1f685a2ae02dde962f30e868e1d9760f10723ef
NEBULA1978 <rpascualey@gmail.com>
2023-02-27 01:04:43 +0100

    Añadiendo a archivo ./script-dinamicos2-refactorizando2.sh un array para las Flags

--- bash/script-dinamicos2-refactorizando2.sh
+++ bash/script-dinamicos2-refactorizando2.sh
@@ -10,6 +10,9 @@ flag4=false
 flag5=false
 flag6=false
 
+# Array con las opciones válidas para las banderas
+valid_flags=("T" "t" "F" "f")
+
 # Pide al usuario ingresar su nombre
 read -p "INgresa tu nombre: " username
 
@@ -19,44 +22,76 @@ read -p "INgresa tu edad: " userage
 # Pide al usuario ingresar el estado de flag3
 read -p "INgresa el estado de flag3 (T/t o F/f): " flag3
 
-# Convierte la entrada del usuario a mayúsculas con tr y luego la compara con "T"
+# Convierte la entrada del usuario a mayúsculas con tr
 flag3=$(echo $flag3 | tr '[:lower:]' '[:upper:]')
-if [[ $flag3 == "T" ]]; then
-  flag3=true
+
+# Verifica si la entrada del usuario está en el array de opciones válidas
+if [[ " ${valid_flags[*]} " =~ " ${flag3} " ]]; then
+  # Si la entrada es "T", establece flag3 como verdadera
+  if [[ $flag3 == "T" ]]; then
+    flag3=true
+  else
+    flag3=false
+  fi
 else
+  # Si la entrada no es válida, establece flag3 como falsa
   flag3=false
 fi
 
 # Pide al usuario ingresar el estado de flag4
 read -p "INgresa el estado de flag4 (T/t o F/f): " flag4
 
-# Convierte la entrada del usuario a mayúsculas con tr y luego la compara con "T"
+# Convierte la entrada del usuario a mayúsculas con tr
 flag4=$(echo $flag4 | tr '[:lower:]' '[:upper:]')
-if [[ $flag4 == "T" ]]; then
-  flag4=true
+
+# Verifica si la entrada del usuario está en el array de opciones válidas
+if [[ " ${valid_flags[*]} " =~ " ${flag4} " ]]; then
+  # Si la entrada es "T", establece flag4 como verdadera
+  if [[ $flag4 == "T" ]]; then
+    flag4=true
+  else
+    flag4=false
+  fi
 else
+  # Si la entrada no es válida, establece flag4 como falsa
   flag4=false
 fi
 
 # Pide al usuario ingresar el estado de flag5
 read -p "INgresa el estado de flag5 (T/t o F/f): " flag5
 
-# Convierte la entrada del usuario a mayúsculas con tr y luego la compara con "T"
+# Convierte la entrada del usuario a mayúsculas con tr
 flag5=$(echo $flag5 | tr '[:lower:]' '[:upper:]')
-if [[ $flag5 == "T" ]]; then
-  flag5=true
+
+# Verifica si la entrada del usuario está en el array de opciones válidas
+if [[ " ${valid_flags[*]} " =~ " ${flag5} " ]]; then
+  # Si la entrada es "T", establece flag5 como verdadera
+  if [[ $flag5 == "T" ]]; then
+    flag5=true
+  else
+    flag5=false
+  fi
 else
+  # Si la entrada no es válida, establece flag5 como falsa
   flag5=false
 fi
 
 # Pide al usuario ingresar el estado de flag6
 read -p "INgresa el estado de flag6 (T/t o F/f): " flag6
 
-# Convierte la entrada del usuario a mayúsculas con tr y luego la compara con "T"
+# Convierte la entrada del usuario a mayúsculas con tr
 flag6=$(echo $flag6 | tr '[:lower:]' '[:upper:]')
-if [[ $flag6 == "T" ]]; then
-  flag6=true
+
+# Verifica si la entrada del usuario está en el array de opciones válidas
+if [[ " ${valid_flags[*]} " =~ " ${flag6} " ]]; then
+  # Si la entrada es "T", establece flag6 como verdadera
+  if [[ $flag6 == "T" ]]; then
+    flag6=true
+  else
+    flag6=false
+  fi
 else
+  # Si la entrada no es válida, establece flag6 como falsa
   flag6=false
 fi
 
